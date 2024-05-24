0b494f66b13e7fac7ec23741ccadbad53f025a06
Leo Perard <leo.perard@gmail.com>
2024-05-24 05:03:45 -0400

    wrong line commented 🤦

--- lua/custom/plugins/icon-picker.lua
+++ lua/custom/plugins/icon-picker.lua
@@ -8,9 +8,9 @@ return {
 
     local opts = { noremap = true, silent = true }
 
-    vim.keymap.set('n', '<Leader><Leader>i', '<cmd>IconPickerNormal<cr>', opts)
     -- I don't want to overwrite the <leader><leader> keymap and I don't think I'll use them anyways
+    -- vim.keymap.set('n', '<Leader><Leader>i', '<cmd>IconPickerNormal<cr>', opts)
     -- vim.keymap.set('n', '<Leader><Leader>y', '<cmd>IconPickerYank<cr>', opts) --> Yank the selected icon into register
-    -- vim.keymap.set('i', '<C-i>', '<cmd>IconPickerInsert<cr>', opts)
+    vim.keymap.set('i', '<C-i>', '<cmd>IconPickerInsert<cr>', opts)
   end,
 }
