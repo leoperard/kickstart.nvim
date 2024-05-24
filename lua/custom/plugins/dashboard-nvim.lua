57d5d33c71ec40528c72184c9535b0568893c412
Leo Perard <leo.perard@gmail.com>
2024-05-24 06:04:14 -0400

    add a simple dashboard to neovim ✨

---
+++ lua/custom/plugins/dashboard-nvim.lua
@@ -0,0 +1,40 @@
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      theme = 'hyper',
+      config = {
+        week_header = {
+          enable = true,
+        },
+        shortcut = {
+          { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+          {
+            icon = ' ',
+            icon_hl = '@variable',
+            desc = 'Files',
+            group = 'Label',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+          {
+            desc = ' Apps',
+            group = 'DiagnosticHint',
+            action = 'Telescope app',
+            key = 'a',
+          },
+          {
+            desc = ' dotfiles',
+            group = 'Number',
+            action = 'Telescope dotfiles',
+            key = 'd',
+          },
+        },
+        project = { enable = true, limit = 8, icon = '󰊢', label = '', action = 'Telescope find_files cwd=' },
+        mru = { limit = 10, icon = '', label = '', cwd_only = true },
+      },
+    }
+  end,
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+}
